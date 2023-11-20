b27e5e40e9795c9f80e1449299bd3bde8fb7b59b
Mr.Raindrop <53202747+raindrop213@users.noreply.github.com>
2023-11-21 06:34:14 +0800

    v0.3.3

--- src/options.js
+++ src/options.js
@@ -11,10 +11,39 @@ function loadWindowsVoices() {
   });
 }
 
+// 加载 VITS TTS 语音
+function loadVitsVoices() {
+  // 使用 fetch API 从指定的路径获取 voice.json 文件
+  fetch('src/voice.json')
+    .then(response => response.json()) // 将响应解析为 JSON
+    .then(data => {
+      // 获取 HTML 中的选择框元素
+      const vitsVoiceSelect = document.getElementById('vitsVoice');
+
+      // VITS 语音数据遍历
+      data.VITS.forEach(voice => {
+        // 创建每个语音的选项
+        let option = document.createElement('option');
+        option.text = `${voice.id}_${voice.lang.join('/')}_${voice.name}`;
+        option.value = voice.id;
+
+        // 将选项添加到选择框中
+        vitsVoiceSelect.add(option);
+      });
+    })
+    .catch(error => {
+      console.error('加载 VITS 语音时出错:', error);
+    });
+}
+
+
+
+
 // 加载保存的设置
 function loadSettings() {
   chrome.storage.local.get(['ignoreRT','useVITS', 'voiceName', 'rate', 'pitch', 'clipAPI', 'vitsAPI', 'vitsVoice', 'length', 'noise', 'noisew', 'max', 'streaming'], (data) => {
     loadWindowsVoices(); // 加载 Windows TTS 语音选项
+    loadVitsVoices();
     document.getElementById('ignoreRT').checked = data.ignoreRT || true;
     document.getElementById('useVITS').checked = data.useVITS || false;
     document.getElementById('voiceName').value = data.voiceName || 'Microsoft Sayaka - Japanese (Japan)';
